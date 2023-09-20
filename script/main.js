2f05ab964a89898792340c58e37f1ce2a89f77a5
Twinsherry <1621242531@qq.com>
2023-09-21 01:55:20 +0800

    feat: add JavaScript Practice to Web - 内部藏匿了奇怪的东西，只是用来测试而已

---
+++ script/main.js
@@ -0,0 +1,54 @@
+alert("这只是一个测试界面"+"(*╹▽╹*)艾米卡酱升级js技能中xd");
+
+
+// Just Practice
+console.log("Cat_Over_Flow".length);
+
+console.log("Luttefair".charAt(0));
+
+console.log("Hikari Sakurai".replace("Hikari Sakurai","樱小路露娜"))
+
+console.log("Hikari Sakurai".toLocaleUpperCase())
+
+
+// 布尔类型
+// 比大小 &&与 ||或  !非
+
+// 变量
+
+// var全局 let局部 ？？？
+var name = "Sherry";
+console.log(name);
+name="Amika";
+console.log(name);
+
+let a = 1;
+console.log(a);
+a = a+1;
+console.log(a);
+
+b = 114514;
+console.log(b);
+b = 1919810;
+console.log(b);
+
+var x;
+x = "3" + 4 + 5;
+console.log(x);
+// 全部转换为字符串，数据类型改变
+var henghengaa ;
+henghengaa = 1 + 1 + 4 + 5 + 1 +"4";
+console.log("henghengaa的值是"+henghengaa);
+console.log("味道被清理掉了！");
+
+// 等号
+// 双等号：console.log(123 == "123"); true 数字转换为字符串
+// 1 == true 0 == false
+// 三等号：console.log(123 === "123") false 没有类型自动转换
+
+
+console.log(0 == false); 
+console.log(0 != false);
+console.log(0 !== false);
+console.log(0 === false);
+
